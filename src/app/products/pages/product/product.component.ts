2afc8ef939c6b37eb31fa242fd88ee56a90ac6cc
Andy Castle <masterchief_and@live.com>
2025-01-30 13:02:58 -0500

    configuracion inicial del proyecto de lifecycle

---
+++ src/app/products/pages/product/product.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products-product-pagec',
+  templateUrl: './product.component.html',
+  styles: [],
+})
+export class ProductComponent {}
